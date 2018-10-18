b02d1e0f11eff7708ec65c10f98f18d146d7cebb
wangyonghui <2930807240@qq.com>
2018-10-18 22:37:04 +0800

    springmvc上传图片和富文本上传图片

--- src/main/java/com/mmall/service/impl/FileServiceImpl.java
+++ src/main/java/com/mmall/service/impl/FileServiceImpl.java
@@ -1,8 +1,11 @@
 package com.mmall.service.impl;
 
+import com.google.common.collect.Lists;
 import com.mmall.service.IFileService;
+import com.mmall.util.FTPUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
@@ -12,10 +15,12 @@ import java.util.UUID;
 /**
  * @author 汪永晖
  */
+@Service("iFileService")
 public class FileServiceImpl implements IFileService {
 
     private Logger logger = LoggerFactory.getLogger(FileServiceImpl.class);
 
+    @Override
     public String upload(MultipartFile file, String path) {
         String fileName = file.getOriginalFilename();
         //扩展名
@@ -37,9 +42,8 @@ public class FileServiceImpl implements IFileService {
             file.transferTo(targetFile);
             //文件上传成功
 
-            //todo 将targetFile上传到FTP服务器上
-
-            //todo 上传完成后，删除upload下面的文件
+            FTPUtil.uploadFile(Lists.newArrayList(targetFile));
+            targetFile.delete();
         } catch (IOException e) {
             logger.error("上传文件异常", e);
             return null;
